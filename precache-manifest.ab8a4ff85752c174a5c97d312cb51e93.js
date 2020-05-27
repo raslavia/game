7b120f460edf02e3854242c5e0480e3a6cb2ee48
raslavia <raslavi.work@gmai.com>
2020-05-27 15:04:41 +0300

    Updates

---
+++ precache-manifest.ab8a4ff85752c174a5c97d312cb51e93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5936b09fff6d5a0d5af1ab40ba874f39",
+    "url": "/game/index.html"
+  },
+  {
+    "revision": "4ac28123371329104f15",
+    "url": "/game/static/css/main.e0a0398b.chunk.css"
+  },
+  {
+    "revision": "26e53ee69fb2427fa33b",
+    "url": "/game/static/js/2.37da4eb5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/game/static/js/2.37da4eb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ac28123371329104f15",
+    "url": "/game/static/js/main.4a135953.chunk.js"
+  },
+  {
+    "revision": "f64785df783d3cf53971",
+    "url": "/game/static/js/runtime-main.3e68341f.js"
+  }
+]);
